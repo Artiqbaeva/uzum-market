d68ce518924b774414e705df723ac808ed5b4a46
Artiqbaeva <artiqbaevamafruza@gmail.com>
2025-05-01 23:51:52 +0500

    uzum market

---
+++ script.js
@@ -0,0 +1,24 @@
+const hop = document.querySelector(".latest-watch-wrapper")
+
+function getProduct(){
+    fetch("https://dummyjson.com/products")
+    .then((res)=>{
+        res.json()
+                .then((data)=>{
+                    setProduct(data.products);
+                })
+    })
+}
+
+function setProduct(array){
+
+    for(let i = 0; i < array.length; i++){
+         hop.innerHTML += ` <div class="latest-watch">
+                        <img src="${array[i].images[0]}" alt="">
+                        <h4>${array[i].title}</h4>
+                        <span class="horizontal">${array[i].price}</span><span>$255.00
+                        </span>
+                    </div>`
+    }
+}
+getProduct()
