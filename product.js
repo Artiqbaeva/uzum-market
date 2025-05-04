c2cbf05f9b4e2bb33a3218fc3db719e9e2cdeb5d
Artiqbaeva <artiqbaevamafruza@gmail.com>
2025-05-05 01:33:21 +0500

    edit

---
+++ product.js
@@ -0,0 +1,21 @@
+const params = new URLSearchParams(location.search);
+const box = document.querySelector(".box");
+
+function getProductWithId(id){
+    fetch(`https://dummyjson.com/products/${id}`)
+    .then((res)=>{
+        res.json()
+                .then((data)=>{
+                    box.innerHTML = `
+                    <img class="product-img" src="${data.images[0]}" alt="">
+                    <h4>${data.title}</h4>
+                    <span class="horizontal">${data.price}</span><span>$255.00
+                    </span>
+                    <br>`
+                })
+    })
+}
+window.onload = () => {
+    const id = params.get("id");
+    getProductWithId(id);
+}
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const product = document.querySelector(".latest-watch-wrapper")
+const skeletonEl = document.querySelector(".skeleton")
+const wrapperEl = document.querySelector(".wrapper")
+const seeMore = document.querySelector(".btn-seemore")
+let limitEl = 10;
+
+window.onload = () => {
+    getProduct()
+}
 
 function getProduct(){
-    fetch("https://dummyjson.com/products")
+    renderSkeleton(8);
+    fetch(`https://dummyjson.com/products?limit=${limitEl}`)
     .then((res)=>{
         res.json()
                 .then((data)=>{
@@ -10,14 +19,48 @@ function getProduct(){
     })
 }
 function setProduct(array){
+    skeletonEl.innerHTML = "";
+
     for(let i = 0; i < array.length; i++){
-        product.innerHTML += ` <div class="latest-watch">
-                        <img src="${array[i].images[0]}" alt="">
+        let latestWatch = document.createElement("div");
+        latestWatch.className = "latest-watch";
+        latestWatch.innerHTML = `
+                        <img class="product-img" src="${array[i].images[0]}" alt="">
                         <h4>${array[i].title}</h4>
                         <span class="horizontal">${array[i].price}</span><span>$255.00
                         </span>
-                        <br>
-                    </div>`
+                        <br>`
+        latestWatch.dataset.id = array[i].id;
+
+        product.appendChild(latestWatch);
+        
     }
 }
-getProduct()
+function renderSkeleton(count){
+    const fragment = document.createDocumentFragment()
+    Array(count).fill("").forEach(()=>{
+        let skeletonItem = document.createElement("div")
+        skeletonItem.className = "skeleton__item"
+        skeletonItem.innerHTML = `
+            <div class="skeleton__image skeleton__animation "></div>
+            <div class="skeleton__text skeleton__animation"></div>
+            <div class="skeleton__text skeleton__animation"></div>
+        `
+        fragment.appendChild(skeletonItem)
+    })
+    skeletonEl.appendChild(fragment)
+}
+
+
+product.addEventListener("click", (e)=>{
+    if(e.target.className == "product-img"){
+        const id = e.target.closest(".latest-watch").dataset.id;
+        open(`/product.html?id=${id}`,"_self");
+    }
+})
+
+seeMore.addEventListener('click', (e) => {
+    limitEl += 10;
+    product.innerHTML = null;
+    getProduct();
+})
